cd5cf028e6e19ddf6d77157d20ca7f4cca3ec489
nathan <nathan@cameraforensics.com>
2022-04-19 20:44:35 +0100

    Added support for linux

--- pipedream.rb
+++ pipedream.rb
@@ -1,13 +1,21 @@
 class Pipedream < Formula
     desc "CLI utility for Pipedream"
     homepage "https://pipedream.com"
-    url "https://cli.pipedream.com/darwin/amd64/0.2.5/pd.zip"
-    sha256 "bd1a23ce2428c0c2e35eb80cf69a6069f70d11f1389a9656a27f8a7c1bdf5f9f"
-  
+
+    on_macos do
+      url "https://cli.pipedream.com/darwin/amd64/0.2.5/pd.zip"
+      sha256 "bd1a23ce2428c0c2e35eb80cf69a6069f70d11f1389a9656a27f8a7c1bdf5f9f"    
+    end
+
+    on_linux do
+      url "https://cli.pipedream.com/linux/amd64/0.2.5/pd.zip"
+      sha256 "50435e78aa93db6490896bea93597f0f0a02a619a9cd6404e47cc757fc1e6279"    
+    end
+
     def install
       bin.install "pd"
-    end
-  
+    end  
+
     def caveats; <<~EOS
       ❤ Thanks for installing the Pipedream CLI! If this is your first time using the CLI, be sure to run `pd login` first.
     EOS
