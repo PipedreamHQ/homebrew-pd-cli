793b1cf7d201a7fbf2ecea1487b8735b380ab35b
Boris Verkhovskiy <boris@pipedream.com>
2026-05-27 18:01:36 -0600

    Fix indentation

--- pipedream.rb
+++ pipedream.rb
@@ -1,63 +1,62 @@
 class Pipedream < Formula
-    desc "CLI utility for Pipedream"
-    homepage "https://pipedream.com"
+  desc "CLI utility for Pipedream"
+  homepage "https://pipedream.com"
 
-    def self.fetch_version
-      require "open-uri"
-      URI.open("https://cli.pipedream.com/LATEST_VERSION", &:read).strip
-    rescue
-      "latest"
+  def self.fetch_version
+    require "open-uri"
+    URI.open("https://cli.pipedream.com/LATEST_VERSION", &:read).strip
+  rescue
+    "latest"
+  end
+
+  def self.fetch_checksum(url)
+    require "open-uri"
+    URI.open("#{url}.sha256", &:read).strip
+  rescue
+    # Fallback to :no_check if checksum URL is unavailable
+    :no_check
+  end
+
+  version fetch_version
+
+  on_macos do
+    on_intel do
+      url "https://cli.pipedream.com/darwin/amd64/latest/pd.zip"
+      sha256 fetch_checksum("https://cli.pipedream.com/darwin/amd64/latest/pd.zip")
     end
-
-    def self.fetch_checksum(url)
-      require "open-uri"
-      URI.open("#{url}.sha256", &:read).strip
-    rescue
-      # Fallback to :no_check if checksum URL is unavailable
-      :no_check
+    on_arm do
+      url "https://cli.pipedream.com/darwin/arm64/latest/pd.zip"
+      sha256 fetch_checksum("https://cli.pipedream.com/darwin/arm64/latest/pd.zip")
     end
+  end
 
-    version fetch_version
-
-    on_macos do
-      on_intel do
-        url "https://cli.pipedream.com/darwin/amd64/latest/pd.zip"
-        sha256 fetch_checksum("https://cli.pipedream.com/darwin/amd64/latest/pd.zip")
-      end
-      on_arm do
-        url "https://cli.pipedream.com/darwin/arm64/latest/pd.zip"
-        sha256 fetch_checksum("https://cli.pipedream.com/darwin/arm64/latest/pd.zip")
-      end
+  on_linux do
+    on_intel do
+      url "https://cli.pipedream.com/linux/amd64/latest/pd.zip"
+      sha256 fetch_checksum("https://cli.pipedream.com/linux/amd64/latest/pd.zip")
     end
-
-    on_linux do
-      on_intel do
-        url "https://cli.pipedream.com/linux/amd64/latest/pd.zip"
-        sha256 fetch_checksum("https://cli.pipedream.com/linux/amd64/latest/pd.zip")
-      end
-      on_arm do
-        url "https://cli.pipedream.com/linux/arm64/latest/pd.zip"
-        sha256 fetch_checksum("https://cli.pipedream.com/linux/arm64/latest/pd.zip")
-      end
+    on_arm do
+      url "https://cli.pipedream.com/linux/arm64/latest/pd.zip"
+      sha256 fetch_checksum("https://cli.pipedream.com/linux/arm64/latest/pd.zip")
     end
+  end
 
-    # Automatically check for new versions
-    livecheck do
-      url "https://cli.pipedream.com/LATEST_VERSION"
-      regex(/^v?(\d+(?:\.\d+)+)$/i)
-    end
+  # Automatically check for new versions
+  livecheck do
+    url "https://cli.pipedream.com/LATEST_VERSION"
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
 
-    def install
-      bin.install "pd"
-    end  
+  def install
+    bin.install "pd"
+  end
 
-    def caveats; <<~EOS
-      ❤ Thanks for installing the Pipedream CLI! If this is your first time using the CLI, be sure to run `pd login` first.
-    EOS
-    end
-  
-    test do
-      system "#{bin}/pd", "--version"
-    end
+  def caveats; <<~EOS
+    ❤ Thanks for installing the Pipedream CLI! If this is your first time using the CLI, be sure to run `pd login` first.
+  EOS
+  end
+
+  test do
+    system "#{bin}/pd", "--version"
+  end
 end
-
